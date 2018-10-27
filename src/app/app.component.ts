41bbae827cd9b6d4d5b825ee942b8ad13af0ab86
alexho <Qwedf@bs.com>
2018-10-27 23:51:06 +0800

    iuoiyiyi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {CookiesService} from './cookies.service';
+import {LoginService} from './login.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,15 @@ export class AppComponent {
   title = 'app';
   loggedin = false;
   user: any;
-  constructor(private cookies: CookiesService) {
+  constructor(private cookies: CookiesService, private login:LoginService) {
     if (this.cookies.getCookie('usr') != '') {
       this.user = (JSON.parse(this.cookies.getCookie('usr')));
       if (this.user.ID > 0) {
         this.loggedin = true;
       }
     }
+  }
+  logout(){
+    this.login.logout();
   }
 }
