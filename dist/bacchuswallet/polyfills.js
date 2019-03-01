60581b2eed187718704ae6e947dad16485b90c24
Alexander Ho <usagii@link.cuhk.edu.hk>
2019-03-01 18:13:53 +0800

    build 2x

--- dist/bacchuswallet/polyfills.js
+++ dist/bacchuswallet/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/alexho/WebstormProjects/bacchuswallet/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\user\bacchuswallet\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
